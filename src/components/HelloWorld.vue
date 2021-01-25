26f0f59babb516b1326a2699cd6b19dcb43c573e
Fahim ahamed <54832397+FahimAhamed101@users.noreply.github.com>
2021-01-25 01:32:43 -0800

    Add files via upload
    added

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,305 @@
+<template>
+  <v-container>
+  <v-card
+    class="mx-auto"
+    max-width="100%"
+  >
+  
+    <v-container>
+     
+         <v-row
+    align="center"
+    justify="space-around"
+  ><v-col> <v-btn block >Normal</v-btn></v-col>
+   
+    <v-col> <v-btn block >Normal</v-btn></v-col>
+   <v-col> <v-btn block >Normal</v-btn></v-col>
+    <v-col> <v-btn block >Normal</v-btn></v-col>
+  </v-row><v-row
+    align="center"
+    justify="space-around"
+  ><v-col> <v-btn block >Normal</v-btn></v-col>
+   
+    <v-col> <v-btn block >Normal</v-btn></v-col>
+   <v-col> <v-btn block >Normal</v-btn></v-col>
+    <v-col> <v-btn block >Normal</v-btn></v-col>
+  </v-row>
+    </v-container><v-divider></v-divider>
+<v-container>
+     
+         <v-row
+    align="center"
+    justify="space-around"
+  ><v-col> <v-btn block >Normal</v-btn></v-col>
+   
+    <v-col> <v-btn block >Normal</v-btn></v-col>
+   <v-col> <v-btn block >Normal</v-btn></v-col>
+    <v-col> <v-btn block >Normal</v-btn></v-col>
+  </v-row><v-row
+    align="center"
+    justify="space-around"
+  ><v-col> <v-btn block >Normal</v-btn></v-col>
+   
+    <v-col> <v-btn block >Normal</v-btn></v-col>
+   <v-col> <v-btn block >Normal</v-btn></v-col>
+    <v-col> <v-btn block >Normal</v-btn></v-col>
+  </v-row>
+    </v-container><v-divider></v-divider><v-container fluid>
+      <v-row justify="space-around">
+        <v-col
+          
+          cols="3"
+        >
+          <v-card><v-card-title class="justify-center">Pre-fab homes</v-card-title>
+            <v-img
+              src="https://cdn.vuetifyjs.com/images/cards/house.jpg"
+              class="white--text align-end"
+              gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+              height="200px"
+            >
+              
+            </v-img>
+
+            <v-card-actions>
+              <v-spacer></v-spacer>
+
+              <v-btn icon>
+                <v-icon>mdi-heart</v-icon>
+              </v-btn>
+
+              <v-btn icon>
+                <v-icon>mdi-bookmark</v-icon>
+              </v-btn>
+
+              <v-btn icon>
+                <v-icon>mdi-share-variant</v-icon>
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col><v-col
+          
+          cols="3"
+        >
+          <v-card><v-card-title class="justify-center">Pre-fab homes</v-card-title>
+            <v-img
+              src="https://cdn.vuetifyjs.com/images/cards/house.jpg"
+              class="white--text align-end"
+              gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+              height="200px"
+            >
+              
+            </v-img>
+
+            <v-card-actions>
+              <v-spacer></v-spacer>
+
+              <v-btn icon>
+                <v-icon>mdi-heart</v-icon>
+              </v-btn>
+
+              <v-btn icon>
+                <v-icon>mdi-bookmark</v-icon>
+              </v-btn>
+
+              <v-btn icon>
+                <v-icon>mdi-share-variant</v-icon>
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col><v-col
+          
+          cols="3"
+        >
+          <v-card><v-card-title class="justify-center">Pre-fab homes</v-card-title>
+            <v-img
+              src="https://cdn.vuetifyjs.com/images/cards/house.jpg"
+              class="white--text align-end"
+              gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+              height="200px"
+            >
+              
+            </v-img>
+
+            <v-card-actions>
+              <v-spacer></v-spacer>
+
+              <v-btn icon>
+                <v-icon>mdi-heart</v-icon>
+              </v-btn>
+
+              <v-btn icon>
+                <v-icon>mdi-bookmark</v-icon>
+              </v-btn>
+
+              <v-btn icon>
+                <v-icon>mdi-share-variant</v-icon>
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col><v-col
+          
+          cols="3"
+        >
+          <v-card><v-card-title class="justify-center">Pre-fab homes</v-card-title>
+            <v-img
+              src="https://cdn.vuetifyjs.com/images/cards/house.jpg"
+              class="white--text align-end"
+              gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+              height="200px"
+            >
+              
+            </v-img>
+
+            <v-card-actions>
+              <v-spacer></v-spacer>
+
+              <v-btn icon>
+                <v-icon>mdi-heart</v-icon>
+              </v-btn>
+
+              <v-btn icon>
+                <v-icon>mdi-bookmark</v-icon>
+              </v-btn>
+
+              <v-btn icon>
+                <v-icon>mdi-share-variant</v-icon>
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row><v-row justify="space-around">
+        <v-col
+          
+          cols="3"
+        >
+          <v-card><v-card-title class="justify-center">Pre-fab homes</v-card-title>
+            <v-img
+              src="https://cdn.vuetifyjs.com/images/cards/house.jpg"
+              class="white--text align-end"
+              gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+              height="200px"
+            >
+              
+            </v-img>
+
+            <v-card-actions>
+              <v-spacer></v-spacer>
+
+              <v-btn icon>
+                <v-icon>mdi-heart</v-icon>
+              </v-btn>
+
+              <v-btn icon>
+                <v-icon>mdi-bookmark</v-icon>
+              </v-btn>
+
+              <v-btn icon>
+                <v-icon>mdi-share-variant</v-icon>
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col><v-col
+          
+          cols="3"
+        >
+          <v-card><v-card-title class="justify-center">Pre-fab homes</v-card-title>
+            <v-img
+              src="https://cdn.vuetifyjs.com/images/cards/house.jpg"
+              class="white--text align-end"
+              gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+              height="200px"
+            >
+              
+            </v-img>
+
+            <v-card-actions>
+              <v-spacer></v-spacer>
+
+              <v-btn icon>
+                <v-icon>mdi-heart</v-icon>
+              </v-btn>
+
+              <v-btn icon>
+                <v-icon>mdi-bookmark</v-icon>
+              </v-btn>
+
+              <v-btn icon>
+                <v-icon>mdi-share-variant</v-icon>
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col><v-col
+          
+          cols="3"
+        >
+          <v-card><v-card-title class="justify-center">Pre-fab homes</v-card-title>
+            <v-img
+              src="https://cdn.vuetifyjs.com/images/cards/house.jpg"
+              class="white--text align-end"
+              gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+              height="200px"
+            >
+              
+            </v-img>
+
+            <v-card-actions>
+              <v-spacer></v-spacer>
+
+              <v-btn icon>
+                <v-icon>mdi-heart</v-icon>
+              </v-btn>
+
+              <v-btn icon>
+                <v-icon>mdi-bookmark</v-icon>
+              </v-btn>
+
+              <v-btn icon>
+                <v-icon>mdi-share-variant</v-icon>
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col><v-col
+          
+          cols="3"
+        >
+          <v-card><v-card-title class="justify-center" >Pre-fab homes</v-card-title>
+            <v-img
+              src="https://cdn.vuetifyjs.com/images/cards/house.jpg"
+              class="white--text align-end"
+              gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+              height="200px"
+            >
+              
+            </v-img>
+
+            <v-card-actions>
+              <v-spacer></v-spacer>
+
+              <v-btn icon>
+                <v-icon>mdi-heart</v-icon>
+              </v-btn>
+
+              <v-btn icon>
+                <v-icon>mdi-bookmark</v-icon>
+              </v-btn>
+
+              <v-btn icon>
+                <v-icon>mdi-share-variant</v-icon>
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-card>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'HelloWorld',
+
+    data: () => ({ 
+    }),
+  }
+</script>
